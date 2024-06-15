a8c0f9a6f792febba248fecadc2e64f3671f1549
Lucas Machado <lucasemmanoel@gmail.com>
2024-06-15 02:52:59 -0300

    Configurado Sanctum e Contruido primeira etapa do Login e Cadastro de Usuário

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function register(RegisterUserRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+}
